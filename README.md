9fbdf3e7cdca1a07e9e70dac2c3a3ae046c7c5fa
Chris Fregly <dev@chrisfregly.com>
2023-09-13 14:33:37 -0700

    use quibbble/boardgame components

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
-# Indigo
+# Tsuro
 
-Indigo website that makes use of the quibbble backend. View at https://indigo.quibbble.com.
+[![Netlify Status](https://api.netlify.com/api/v1/badges/3d12a9ad-1a6c-4556-b0a8-5ac7b26a4f68/deploy-status)](https://app.netlify.com/sites/indigo-quibbble/deploys)
+
+Indigo game website. Play at [indigo.quibbble.com](https://indigo.quibbble.com).
+
+This repo contains [ReactJS](https://react.dev) frontend code and makes use of custom React components found at [boardgame](https://github.com/quibbble/boardgame). Game logic can be found at [go-indigo](https://github.com/quibbble/go-indigo). Server logic can be found at [go-quibbble](https://github.com/quibbble/go-quibbble). 
+
+[![Quibbble Indigo](screenshot.png)](https://indigo.quibbble.com)
+
+## Run Locally
+
+- Generate a personal `GITHUB_ACCESS_TOKEN` with package read permissions. Read more about it [here](https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-npm-registry).
+- Create a `.npmrc` file in the `indigo` root director with the following:
+```
+//npm.pkg.github.com/:_authToken=<GITHUB_ACCESS_TOKEN>
+@quibbble:boardgame=https://npm.pkg.github.com
+```
+- Run `npm i`.
+- Run the quibbble server ([go-quibbble](https://github.com/quibbble/go-quibbble)) locally on port `8080`.
+- Create a `.env.local` file in the `indigo` root directory with the following:
+```
+REACT_APP_HOST="http://127.0.0.1:8080"
+REACT_APP_WEBSOCKET="ws://127.0.0.1:8080"
+```
+- Run `npm start`.
---
+++ src/components/game/Game.jsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState, forwardRef } from "react";
+import { BsArrowClockwise, BsArrowUp } from "react-icons/bs";
+import { DndProvider } from "react-dnd";
+import DropSpace from "./DropSpace";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Tile from "./Tile";
+import { TouchBackend } from "react-dnd-touch-backend";
+import { isMobile } from "react-device-detect";
+
+export const Game = forwardRef((props, ref) => {
+    // eslint-disable-next-line no-unused-vars
+    const { ws, game, network, chat, connected, error } = props;
+
+    // websocket messages
+    const placeTile = (team, tile, row, col) => {
+        if (!ws.current) return;
+        ws.current.send(JSON.stringify({"ActionType": "PlaceTile", "Team": team, "MoreDetails": {"Tile": tile, "Row": row, "Column": col}}));
+    }
+
+    const rotateTile = (team, tile) => {
+        if (!ws.current) return;
+        ws.current.send(JSON.stringify({"ActionType": "RotateTileClockwise", "Team": team, "MoreDetails": {"Tile": tile}}));
+    }
+
+    // board resize logic
+    const [tileSize, setTileSize] = useState(0);
+    useEffect(() => {
+        function handleResize() {
+            const width = 9;
+            if (!ref || !ref.current) return;
+            else setTileSize(ref.current.clientWidth/width);
+        }
+        handleResize()
+    });
+    useEffect(() => {
+        function handleResize() {
+            const width = 9;
+            if (!ref || !ref.current) return;
+            else setTileSize(ref.current.clientWidth/width);
+        }
+        window.addEventListener("resize", handleResize);
+        return _ => window.removeEventListener("resize", handleResize)
+    }, [ref]);
+
+    return (
+        <DndProvider backend={ isMobile ? TouchBackend : HTML5Backend }>
+            <div className="h-full flex flex-col justify-center items-center grow">
+                <div className="box-border flex flex-col mb-[5.5rem]" style={{ width: `${ tileSize*9 }px`, height: `${ tileSize*9*0.84 }px` }}>
+                    {
+                        game ? game.MoreData.Board.Tiles.map((row, rIdx) => 
+                            <div key={ rIdx } className="w-full h-full flex items-center justify-center">
+                                {
+                                    row.map((el, cIdx) => 
+                                            <div key={ rIdx + "," + cIdx } className="box-border" style={{ width: `${tileSize}px`, height: `${tileSize*1.1546}px`, marginBottom: `${tileSize*-1}px`, marginRight: `${tileSize*-0.024}px`}}>
+                                                { 
+                                                    el ? 
+                                                        <Tile paths={ el.Paths } row={ rIdx } col={ cIdx } treasure={ el.Treasure } gems={ game.MoreData.Board.Gems } gateways={ game.MoreData.Board.Gateways } team={ null } placeTile={ () => {} }>
+                                                            <DropSpace key={ cIdx } row={ rIdx } col={ cIdx } tileSize={ tileSize } />
+                                                        </Tile> : 
+                                                        <Tile paths={ null } row={ rIdx } col={ cIdx } treasure={ false } gems={ null } gateways={ game.MoreData.Board.Gateways } team={ null } placeTile={ () => {} }>
+                                                            <DropSpace key={ cIdx } row={ rIdx } col={ cIdx } tileSize={ tileSize } />                                   
+                                                        </Tile> 
+                                                }
+                                            </div>) 
+                                }
+                            </div>) : null
+                    }
+                </div>
+
+                <div className="mb-10 w-full flex justify-between items-center" style={{ height: `${tileSize}px` }}>
+                    <div className="flex flex-col items-center text-zinc-400 max-w-[20%]">
+                        <div className="text-xs font-light italic mb-1 text-center">Click tile to rotate</div>
+                        <BsArrowClockwise />    
+                    </div>
+                    <div className="flex gap-8">
+                    {
+                        game && game.MoreData.Variant === "LargeHands" ? [0, 1].map((_, idx) => 
+                            <div key={ "hand" + idx } style={{ width: `${ tileSize*1.2 }px`, height: `${ tileSize*1.1546*1.2 }px` }}>
+                                { 
+                                    game && network && connected && game.MoreData.Hands[connected[network.Name]] && game.MoreData.Hands[connected[network.Name]].length > idx ? 
+                                        <div className="cursor-pointer" onClick={ () => rotateTile(connected[network.Name], game.MoreData.Hands[connected[network.Name]][idx].Paths) }>
+                                            <Tile paths={ game.MoreData.Hands[connected[network.Name]][idx].Paths } row={ -1 } col={ -1 } treasure={ false } gems={ null } team={ connected[network.Name] } placeTile={ placeTile } />
+                                        </div> : 
+                                        <Tile paths={ null } row={ -1 } col={ -1 } treasure={ false } gems={ null } team={ null } placeTile={ null } />
+                                }
+                            </div>) : [0].map((_, idx) => 
+                            <div key={ "hand" + idx } style={{ width: `${ tileSize*1.2 }px`, height: `${ tileSize*1.1546*1.2 }px` }}>
+                                { 
+                                    game && network && connected && game.MoreData.Hands[connected[network.Name]] ? 
+                                        <div className="cursor-pointer" onClick={ () => rotateTile(connected[network.Name], game.MoreData.Hands[connected[network.Name]][idx].Paths) }>
+                                            <Tile paths={ game.MoreData.Hands[connected[network.Name]][idx].Paths } row={ -1 } col={ -1 } treasure={ false } gems={ null } team={ connected[network.Name] } placeTile={ placeTile } />
+                                        </div> : 
+                                        <Tile paths={ null } row={ -1 } col={ -1 } treasure={ false } gems={ null } team={ null } placeTile={ null } />
+
+                                }
+                            </div>)
+                    }
+                    </div>
+                    <div className="flex flex-col items-center text-zinc-400 max-w-[20%]">
+                        <div className="text-xs font-light italic mb-1 text-center">Drag tile to place</div>
+                        <BsArrowUp />    
+                    </div>
+                </div>
+            </div>
+        </DndProvider>
+    )
+})
